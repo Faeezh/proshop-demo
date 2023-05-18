790f042883c8052b27d94e2b86eef2c432032f33
Faeez Hartley <faeez.hartley@gmail.com>
2023-05-18 22:05:19 +0200

    data import error

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
 import products from './data/products.js';
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+connectDB(); // Connect to MongoDB
 
 const app = express();
 
