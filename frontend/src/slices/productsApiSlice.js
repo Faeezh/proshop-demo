4a592849cfab07465b24f2bb8271e2e691e403e6
Faeez Hartley <faeez.hartley@gmail.com>
2023-06-07 17:04:51 +0200

    Get Product Details Endpoint

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -9,9 +9,16 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       }),
       keepUnusedDataFor: 5,
     }),
+    getProductDetails: builder.query({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
 export const {
   useGetProductsQuery,
+  useGetProductDetailsQuery
 } = productsApiSlice;
